65cb304bdef99ade87a5983f98e374f5b00c8790
blowin <dsessence@gmail.com>
2019-02-02 18:07:41 +0300

    NotNull

---
+++ NotNull/NotNull.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NotNull
+{
+  public struct NotNull<T> where T : class
+  {
+    public T Value { get; }
+
+    public NotNull(T val)
+    {
+      if(val == null)
+        throw new ArgumentNullException(nameof(val));
+
+      Value = val;
+    }
+
+    public static explicit operator NotNull<T>(T val)
+    {
+      return new NotNull<T>(val);
+    }
+
+    public static explicit operator T(NotNull<T> notNull)
+    {
+      return notNull.Value;
+    }
+  }
+}
---
+++ NotNull/NotNullEx.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace NotNull
+{
+  public static class NotNullEx
+  {
+    public static NotNull<T> ToNotNull<T>(this T val) where T : class
+    {
+      return new NotNull<T>(val);
+    }
+
+    public static NotNull<T> ToNotNull<T>(this T val, T defaultV) where T : class
+    {
+      return new NotNull<T>(val ?? defaultV);
+    }
+
+    public static NotNull<T> ToNotNull<T>(this T val, Func<T> defaultFactory) where T : class
+    {
+      if (val != null) 
+        return new NotNull<T>(val);
+      
+      if(defaultFactory == null)
+        throw new ArgumentNullException(nameof(defaultFactory));
+        
+      return new NotNull<T>(defaultFactory());
+    }
+  }
+}
